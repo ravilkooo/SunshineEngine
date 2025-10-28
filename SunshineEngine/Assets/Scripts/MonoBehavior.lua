7f4fc22d461c03151667058a284f219a6ea143d1
MaryDeveloper <mari.avi00@mail.ru>
2025-10-28 10:30:41 +0300

    feat: lua script logic
    Added change position for GO with MonoBehavior script

--- SunshineEngine/Assets/Scripts/MonoBehavior.lua
+++ SunshineEngine/Assets/Scripts/MonoBehavior.lua
@@ -1,11 +1,14 @@
 behavior = {}
 
 function behavior:start()
+    self.speedY = 1.0
     print("Behavior started", self.id)
 end
 
 function behavior:update(dt)
     print("Update:", self.id, dt)
+    local tr = self.owner:getTransform()
+    tr.m_position.y = tr.m_position.y - self.speedY * dt
 end
 
 function behavior:destroy()
