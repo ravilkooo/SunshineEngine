a613f6a86d13ed1557c284c649294fc26bd65d15
MaryDeveloper <mari.avi00@mail.ru>
2026-01-16 11:04:02 +0300

    add: parser from wstring to string

--- SunshineEngine/include/Utils/StringHelper.h
+++ SunshineEngine/include/Utils/StringHelper.h
@@ -14,5 +14,6 @@ public:
 
 	static eastl::string ToEASTL(const std::string& str);
 	static std::string ToSTD(const eastl::string& str);
+	static eastl::string WideToString(const eastl::wstring& ws);
 };
 
